2723bff6c2b483cd0c376370e4dd50937d704b54
diegoparradoc96 <diegoparrado.cardozo@hotmail.com>
2024-02-27 09:17:58 -0500

    ...

--- src/components/Dropdown_.tsx
+++ src/components/Dropdown_.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import {
   Dropdown,
   DropdownTrigger,
@@ -7,6 +9,7 @@ import {
   DropdownItem,
   Button,
 } from "@nextui-org/react";
+import { FaCaretDown } from "react-icons/fa";
 
 import { ISelectionBarElements } from "../common/types";
 
@@ -16,28 +19,55 @@ interface IDropdown_props {
 
 const Dropdown_: React.FC<IDropdown_props> = ({ selectionBarElement }) => {
   const { nameButton, sections } = selectionBarElement;
+  const [showMenu, setShowMenu] = useState<boolean>(false);
 
   return (
-    <Dropdown
-      type="listbox"
-      classNames={{ trigger: "border-0" }}
-      disableAnimation={true}
-    >
-      <DropdownTrigger>
-        <Button variant="bordered">{nameButton}</Button>
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Static Actions">
-        {selectionBarElement.sections.map((section, index) => {
-          return (
-            <DropdownSection title="Actions">
-              {section.sectionElements.map((sectionElement, index) => {
-                return <DropdownItem>{sectionElement}</DropdownItem>;
-              })}
-            </DropdownSection>
-          );
-        })}
-      </DropdownMenu>
-    </Dropdown>
+    // <Dropdown
+    //   type="listbox"
+    //   classNames={{ trigger: "border-0" }}
+    //   disableAnimation={true}
+    // >
+    //   <DropdownTrigger>
+    //     <Button variant="bordered">{nameButton}</Button>
+    //   </DropdownTrigger>
+    //   <DropdownMenu aria-label="Static Actions">
+    //     {selectionBarElement.sections.map((section, index) => {
+    //       return (
+    //         <DropdownSection title={section.nameSection}>
+    //           {section.sectionElements.map((sectionElement, index) => {
+    //             return <DropdownItem>{sectionElement}</DropdownItem>;
+    //           })}
+    //         </DropdownSection>
+    //       );
+    //     })}
+    //   </DropdownMenu>
+    // </Dropdown>
+    <div className="col">
+      <div
+        onMouseEnter={() => setShowMenu(true)}
+        onMouseLeave={() => setShowMenu(false)}
+        className="flex row p-2 text-white cursor-pointer items-center"
+      >
+        <p>hola</p>
+        <FaCaretDown size={13} className="ml-1"></FaCaretDown>
+      </div>
+
+      {showMenu && (
+        <div
+          className="bg-[#3e3] p-10 absolute"
+          onMouseEnter={() => setShowMenu(true)}
+          onMouseLeave={() => setShowMenu(false)}
+        >
+          <ul>
+            <li className="hover:text-[#fff] hover:underline decoration-3 cursor-pointer">
+              hola gente
+            </li>
+            <li>hola gente</li>
+            <li>hola gente</li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 
