7e9d599dd7353bef2eb909fb5e7b86374a94e8f5
diegoparradoc96 <diegoparrado.cardozo@hotmail.com>
2024-03-07 13:54:31 -0500

    ...

--- src/components/FootwearCatalog_.tsx
+++ src/components/FootwearCatalog_.tsx
@@ -6,9 +6,13 @@ const FootwearCatalog_ = () => {
   const catalog = [0, 1, 2, 3, 4, 5];
 
   return (
-    <div className="flex row flex-wrap gap-4">
+    <div className="grid grid-cols-4 gap-2 ml-2">
       {catalog.map((shoe, index) => {
-        return <ShoeCard_ key={index} />;
+        return (
+          <div className="col-span-1">
+            <ShoeCard_ key={index} />
+          </div>
+        );
       })}
     </div>
   );
--- src/components/ShoeCard_.tsx
+++ src/components/ShoeCard_.tsx
@@ -6,40 +6,46 @@ const ShoeCard_ = () => {
   return (
     <div
       className="
-    w-64 min-h-[445px] max-h-[445px] p-2
-    border hover:shadow-lg hover:cursor-pointer
+      grid auto-rows-12 w-full min-h-[445px] max-h-[445px] p-2
+      border hover:shadow-lg hover:cursor-pointer
     "
     >
-      <div className="grid grid-cols-1 grid-rows-2">
-        <div className="row-span-1">
-          <Image
-            src="https://calzatodocol.vteximg.com.br/arquivos/ids/216552-292-292/712100069_blanco-verde_02.jpg?v=638376158510500000.jpeg"
-            //className="row-span-1"
-          />
-        </div>        
-        <div className="col-span-2">
-          <p className=" font-bold">Fratini</p>
-        </div>
-        {/* <div className="row-span-1">
-          <p className=" text-gray-400 text-[14px]">
+      <div className="">
+        <Image src="https://calzatodocol.vteximg.com.br/arquivos/ids/216552-292-292/712100069_blanco-verde_02.jpg?v=638376158510500000.jpeg" />
+      </div>
+      <p className="font-bold">Fratini</p>
+      <p className="font-bold">Fratini</p>
+      <p className="font-bold">Fratini</p>
+      <p className="font-bold">Fratini</p>
+      <p className="font-bold">Fratini</p>
+      <p className="font-bold">Fratini</p>
+      <p className="font-bold">Fratini</p>
+      <p className="font-bold">Fratini</p>
+      <p className="font-bold">Fratini</p>
+
+      {/* <div className="">
+        <p className="font-bold">Fratini</p>
+      </div> */}
+      {/* <div className="">
+          <p className="text-gray-400 text-[14px]">
             Mocasin de Mujer marca Frattini
           </p>
-        </div>
-        <div className="row-span-1">
-          <p className=" font-bold text-[10px] text-gray-400">Ahora</p>
-        </div>
-        <div className="row-span-1">
-          <p className="">precio</p>
-        </div>
-        <div className="row-span-1">
-          <p className="">color</p>
-        </div>
-
-        <div className="row-span-1">
-          <p className="text-center">talla</p>
-          <p className="text-center">Agregar al carrito</p>
         </div> */}
-      </div>
+      {/* <div className="">
+        <p className="font-bold text-[10px] text-gray-400">Ahora</p>
+      </div> */}
+
+      {/* <p className="font-bold">$300.000</p> */}
+
+      {/* <div className="mt-2">
+          <p className=" text-[10px] text-gray-400">Color</p>
+          <p className="-mt-1">colores</p>
+        </div> */}
+
+      {/* <div className="grid grid-cols-2 auto-cols-max">
+          <p className="text-center text-xs">talla</p>
+          <p className="text-center text-xs">Agregar al carrito</p>
+        </div> */}
     </div>
   );
 };
